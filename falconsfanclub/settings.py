9828555faf876aa2635b936fa08b9955576c13da
michaelmiles1 <michael.gage.miles@gmail.com>
2019-09-13 17:26:59 -0700

    Add bootstrap to project

--- falconsfanclub/settings.py
+++ falconsfanclub/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'falconsfanclub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["falconsfanclub/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
